ff17610ccbfaf61b02049eddf5ae70ecd49568e9
Stefano <stefano.funaro@gmail.com>
2021-09-22 16:43:33 +0200

    Header-Main-components-created-Disk-component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <img class="m-1" src="../assets/img/spotify.png" alt="spotify icon">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../assets/style/general.scss';
+
+header{
+  background-color: $lighterGray;
+  height: 60px;
+  
+  img{
+    height: 50px;
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="container-fluid">
+      <div class="row">
+          <div class="col-6 col-md-4 col-xl-2">
+              
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'Main',
+    data() {
+        return {
+            APIUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            albums: [],
+
+        }
+    },
+    created() {
+        this.getAlbums();
+    },
+    methods: {
+        getAlbums() {
+
+            axios
+            .get(this.APIUrl)
+            .then (res => {
+                // console.log(res.data)
+                this.albums = res.data.response
+            })
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/general.scss';
+section {
+    background-color: $darkerGray;
+    height: calc(100vh - 60px);
+}
+</style>
